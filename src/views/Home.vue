aa97354041c07433fea3ac1a4d23fac4ebfcdfa3
juandigv69 <juandigv69@gmail.com>
2020-06-01 19:57:11 -0400

    code cleanup

--- src/views/Home.vue
+++ src/views/Home.vue
@@ -1,22 +1,21 @@
 <template>
     <div class="home">
         <span>Library Practice</span>
-        <br />
-        <br />
+        <br>
+        <br>
         <input v-model="name" placeholder="Search" />
         <select v-model="selectedOption">
-          <option>All</option>
-          <option>Books</option>
-          <option>Magazines</option>
-          <option>Newspapers</option>
-          <option>Others</option>
-        </select>
-        <button @click="SearchbyName()">Search by Name</button>
-        <button @click="SearchbyDescription()">Search by Description</button>
-        <br />
-        <br />
-    
-        <table>
+                          <option>All</option>
+                          <option>Books</option>
+                          <option>Magazines</option>
+                          <option>Newspapers</option>
+                          <option>Others</option>
+                        </select>
+        <br>
+        <br>
+        <span>Library by Name</span>
+       
+        <table style="margin: 0px auto">
             <thead>
                 <tr>
                     <th>Code</th>
@@ -34,9 +33,11 @@
                 </tr>
             </tbody>
         </table>
-    
-    
-        <table>
+        <br>
+        <br>
+        <span>Library by Description</span>
+      
+        <table style="margin: 0px auto">
             <thead>
                 <tr>
                     <th>Code</th>
@@ -55,8 +56,7 @@
             </tbody>
         </table>
     
-        <br>
-        <br>
+    
     
     
     </div>
@@ -73,25 +73,71 @@ export default {
             list: [{
                     code: "bk01",
                     name: "Book One",
-                    desc: "Generic description",
+                    desc: "First book of the series",
                     category: "Books"
                 },
                 {
                     code: "np01",
                     name: "Newspaper Manhattan",
-                    desc: "Generic description",
+                    desc: "sad news",
                     category: "Newspapers"
                 },
                 {
                     code: "mz01",
                     name: "Funny Magazine",
-                    desc: "Generic description",
+                    desc: "haha funny word",
                     category: "Magazines"
                 },
                 {
                     code: "ot01",
                     name: "Kronorium",
                     desc: "Generic description",
+                    category: "Others"
+                }, {
+                    code: "bk02",
+                    name: "Book Two",
+                    desc: "Second book of the series",
+                    category: "Books"
+                },
+                {
+                    code: "np02",
+                    name: "Newspaper DC",
+                    desc: "good news",
+                    category: "Newspapers"
+                },
+                {
+                    code: "mz02",
+                    name: "Clay Magazine",
+                    desc: "Clayman approved",
+                    category: "Magazines"
+                },
+                {
+                    code: "ot02",
+                    name: "Bounty Hunter Guide",
+                    desc: "Generic description",
+                    category: "Others"
+                }, {
+                    code: "bk03",
+                    name: "Book Three",
+                    desc: "Third book in the series",
+                    category: "Books"
+                },
+                {
+                    code: "np03",
+                    name: "Newspaper Reeves",
+                    desc: "its just news",
+                    category: "Newspapers"
+                },
+                {
+                    code: "mz03",
+                    name: "PC Magazine",
+                    desc: "rgb",
+                    category: "Magazines"
+                },
+                {
+                    code: "ot03",
+                    name: "Sacred Jedi Texts",
+                    desc: "read them have you",
                     category: "Others"
                 }
             ]
@@ -115,14 +161,6 @@ export default {
                 categoryList.filter(item => item.desc.includes(this.name))
         },
 
-    },
-    methods: {
-        SearchbyName() {
-
-        },
-        SearchbyDescription() {
-
-        }
     }
 };
 </script>
