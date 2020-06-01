77f59c2a31ce176c9255e12ca85929143a0944d6
juandigv69 <juandigv69@gmail.com>
2020-06-01 18:30:02 -0400

    added filters by category and initial search tool

--- src/views/Home.vue
+++ src/views/Home.vue
@@ -1,18 +1,148 @@
 <template>
   <div class="home">
-    <img alt="Vue logo" src="../assets/logo.png" />
-    <HelloWorld msg="Welcome to Your Vue.js App" />
+    <span>Library Practice</span>
+    <br />
+    <br />
+    <input v-model="name" placeholder="Search" />
+    <select v-model="selectedOption">
+      <option>All</option>
+      <option>Books</option>
+      <option>Magazines</option>
+      <option>Newspapers</option>
+      <option>Others</option>
+    </select>
+    <button @click="SearchbyName()">Search by Name</button>
+    <button @click="SearchbyDescription()">Search by Description</button>
+    <br />
+    <br />
+    <div>
+      <table striped hover :list="list"></table>
+    </div>
+
+    <table>
+      <thead>
+        <tr>
+          <th>Code</th>
+          <th>Name</th>
+          <th>Description</th>
+          <th>Category</th>
+        </tr>
+      </thead>
+      <tbody>
+        <tr :key="item.code" v-for="item in filteredListCat">
+          <td>{{item.code}}</td>
+          <td>{{item.name}}</td>
+          <td>{{item.desc}}</td>
+          <td>{{item.category}}</td>
+        </tr>
+      </tbody>
+    </table>
+
+     <br>
+    <br>
+    <table>
+      <thead>
+        <tr>
+          <th>Code</th>
+          <th>Name</th>
+          <th>Description</th>
+          <th>Category</th>
+        </tr>
+      </thead>
+      <tbody>
+        <tr :key="item.code" v-for="item in filteredListDesc">
+          <td>{{item.code}}</td>
+          <td>{{item.name}}</td>
+          <td>{{item.desc}}</td>
+          <td>{{item.category}}</td>
+        </tr>
+      </tbody>
+    </table>
+
+     <br>
+    <br>
+    <table>
+      <thead>
+        <tr>
+          <th>Code</th>
+          <th>Name</th>
+          <th>Description</th>
+          <th>Category</th>
+        </tr>
+      </thead>
+      <tbody>
+        <tr :key="item.code" v-for="item in filteredListName">
+          <td>{{item.code}}</td>
+          <td>{{item.name}}</td>
+          <td>{{item.desc}}</td>
+          <td>{{item.category}}</td>
+        </tr>
+      </tbody>
+    </table>
   </div>
 </template>
 
 <script>
-// @ is an alias to /src
-import HelloWorld from "@/components/HelloWorld.vue";
-
 export default {
   name: "Home",
-  components: {
-    HelloWorld
+  components: {},
+  data() {
+    return {
+      selectedOption: "All",
+      name: "",
+      list: [
+        {
+          code: "lbr01",
+          name: "Book One",
+          desc: "Generic description",
+          category: "Books"
+        },
+        {
+          code: "lbr02",
+          name: "Newspaper Manhattan",
+          desc: "Generic description",
+          category: "Newspapers"
+        },
+        {
+          code: "lbr03",
+          name: "Funny Magazine",
+          desc: "Generic description",
+          category: "Magazines"
+        },
+        {
+          code: "lbr04",
+          name: "Kronorium",
+          desc: "Generic description",
+          category: "Others"
+        }
+      ]
+    };
+  },
+  computed: {
+    filteredListName() {
+      return this.name === ""
+        ? this.list
+        : this.list.filter(item => item.name === this.name);
+    },
+    filteredListDesc() {
+      return this.name === ""
+        ? this.list
+        : this.list.filter(item => item.desc === this.name);
+    },
+    filteredListCat() {
+      return this.selectedOption === "" || this.selectedOption === "All"
+        ? this.list
+        : this.list.filter(item => item.category === this.selectedOption);
+    }
+  },
+  methods: {
+    SearchbyName() {
+      
+    },
+    SearchbyDescription() {
+      
+    }
   }
 };
 </script>
+
